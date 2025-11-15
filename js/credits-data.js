492dec7da9a73876bceb1d9c606cce62e03de745
Looof <156966975+Looofc@users.noreply.github.com>
2025-11-16 00:36:07 +0100

    Create credits-data.js

---
+++ js/credits-data.js
@@ -0,0 +1,20 @@
+const creditsData = {
+  supporters: [
+    { name: "Plutonics",      role: "Supporter" },
+    { name: "Luxxy",          role: "Supporter" },
+    // ...
+  ],
+  ishBuilders: [
+    { name: "Manhattenman",   role: "Builder" },
+    { name: "Gartell",        role: "Builder" },
+    // ...
+  ],
+  creators: [
+    { name: "Jawunleashed",   role: "Creator / Projects" },
+    // ...
+  ],
+  bakery: [
+    { name: "Tetoala",        role: "Builder" },
+    // ...
+  ]
+};
